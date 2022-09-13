380793628ba0a1ae22e77b2147cc4da784ba2992
charlie-daniels <charlied030401@gmail.com>
2022-09-13 21:52:52 +0100

    Reformat files and add home tab

---
+++ src/pages/home.js
@@ -0,0 +1,49 @@
+import chefJpeg from './../images/chef.jpeg';
+
+function createIntroduction() {
+  const intro = document.createElement('section');
+  intro.setAttribute('id', 'introduction');
+
+  const para1 = document.createElement('p');
+  para1.classList.add('text');
+  para1.textContent = 'This is the experience of your lifetime. Taste the hours of labour spent crafting perfection, and have our chefs walk you through the flavour sensation that is our cuisine.';
+  const para2 = document.createElement('p');
+  para2.classList.add('text');
+  para2.textContent = 'Whether it be fellow strangers at the Chef\'s table, or as a group of loved ones, embark on a guided tour through the delectable, untamed and ferocious aromas of our locally sourced ingredients.';
+
+  intro.append(para1, para2);
+  return intro;
+}
+
+function createPreview() {
+  const preview = document.createElement('section');
+  preview.setAttribute('id', 'preview');
+
+  const chef = document.createElement('div');
+  chef.classList.add('chef');
+  preview.appendChild(chef);
+
+  const chefImg = new Image();
+  chefImg.src = chefJpeg;
+  const quoteWrapper = document.createElement('div');
+  quoteWrapper.classList.add('quote');
+  chef.append(chefImg, quoteWrapper);
+
+  const quote = document.createElement('q');
+  quote.textContent = 'My food has been my lifelong dream, and now I can make it other people\'s reality.';
+  const quoteRef = document.createElement('p');
+  quoteRef.textContent = '- Head Chef Pellesi';
+  quoteWrapper.append(quote, quoteRef);
+
+  return preview;
+}
+
+export default function loadHome() {
+  const home = document.createElement('div');
+  
+  const intro = createIntroduction();
+  const preview = createPreview();
+
+  home.append(intro, preview);
+  return home;
+}
--- src/pages/website.js
+++ src/pages/website.js
@@ -1,6 +1,5 @@
-import chefJpeg from './../images/chef.jpeg';
-import backgroundJpeg from './../images/background.jpeg';
 import style from './../styles/style.css';
+import loadHome from './home.js';
 
 function createHeader() {
   const header = document.createElement('header');
@@ -12,56 +11,26 @@ function createHeader() {
 }
 
 function createNav() {
+  const loadPage = (page) => {
+    const container = document.getElementById('content');
+    const footer = document.querySelector('footer');
+    container.insertBefore(page, footer);
+  }
+
   const nav = document.createElement('nav');
 
-  const home = document.createElement('a');
+  const home = document.createElement('button');
   home.textContent = 'Home';
-  const menu = document.createElement('a');
+  home.addEventListener('click', () => loadPage(loadHome()));
+  const menu = document.createElement('button');
   menu.textContent = 'Menu';
-  const contact = document.createElement('a');
+  const contact = document.createElement('button');
   contact.textContent = 'Contact';
 
   nav.append(home, menu, contact);
   return nav;
 }
 
-function createIntroduction() {
-  const intro = document.createElement('section');
-  intro.setAttribute('id', 'introduction');
-
-  const para1 = document.createElement('p');
-  para1.classList.add('text');
-  para1.textContent = 'This is the experience of your lifetime. Taste the hours of labour spent crafting perfection, and have our chefs walk you through the flavour sensation that is our cuisine.';
-  const para2 = document.createElement('p');
-  para2.classList.add('text');
-  para2.textContent = 'Whether it be fellow strangers at the Chef\'s table, or as a group of loved ones, embark on a guided tour through the delectable, untamed and ferocious aromas of our locally sourced ingredients.';
-
-  intro.append(para1, para2);
-  return intro;
-}
-
-function createPreview() {
-  const preview = document.createElement('section');
-  preview.setAttribute('id', 'preview');
-
-  const chef = document.createElement('div');
-  chef.classList.add('chef');
-  preview.appendChild(chef);
-
-  const chefImg = new Image();
-  chefImg.src = chefJpeg;
-  const quoteWrapper = document.createElement('div');
-  quoteWrapper.classList.add('quote');
-  chef.append(chefImg, quoteWrapper);
-
-  const quote = document.createElement('q');
-  quote.textContent = 'My food has been my lifelong dream, and now I can make it other people\'s reality.';
-  const quoteRef = document.createElement('p');
-  quoteRef.textContent = '- Head Chef Pellesi';
-  quoteWrapper.append(quote, quoteRef);
-
-  return preview;
-}
 
 function createFooter() {
   const footer = document.createElement('footer');
@@ -74,24 +43,19 @@ function createFooter() {
   return footer;
 }
 
-
 export default function initialiseWebsite() {
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
 
   const header = createHeader();
   const nav = createNav();
-  const intro = createIntroduction();
-  const preview = createPreview();
   const footer = createFooter();
 
   content.append(
     header,
     nav,
-    intro,
-    preview,
     footer
   );
-
+  
   document.querySelector('body').appendChild(content);
 }
